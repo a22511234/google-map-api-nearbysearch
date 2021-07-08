967cdc0fcdc9d3788ec3616ce9539d36e646950c
黃子豪 <a22511234@gmail.com>
2021-07-08 11:42:08 +0800

    0708 main
    使用Google map api 進行 HTTP CLIENT 進行 LBS 服務

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            google_map_api("", "25.022638619751678","121.47251498411259");
+
+        }
+        static void google_map_api(string userid ,string lat,string lng)
+        {
+            string sURL = $"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lng}&radius=500&type=supermarket&keyword=%E9%A4%90%E5%BB%B3&key=AIzaSyBGAICU-ZH4QYDFSZW4jzgo9DWARe_pPrM";
+            using (var client = new WebClient())
+            using (var stream = client.OpenRead(sURL))
+            using (var reader = new StreamReader(stream))
+            using (var json = new JsonTextReader(reader))
+            {
+                var jObject = Newtonsoft.Json.Linq.JObject.Load(json);
+                string location0_name = (string)jObject["results"][0]["name"];
+                string location0_lat = (string)jObject["results"][0]["geometry"]["location"]["lat"];
+                string location0_lng = (string)jObject["results"][0]["geometry"]["location"]["lng"];
+                string location0_placeID = (string)jObject["results"][0]["place_id"];
+
+            }
+        }
+        
+    }
+}
